bcf034298ce61443da68799fa7f03d2ded9c72b5
punyeon <kevin.lle1099@gmail.com>
2021-01-15 16:26:40 -0600

    updated home-page

--- financial_planner/budgeter/urls.py
+++ financial_planner/budgeter/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='budgeter-home')
+    path('', views.home, name='budgeter-home'),
+    path('about/', views.about, name='budgeter-about'),
+    path('contact/', views.contact, name='budgeter-contact')
 ]
--- financial_planner/budgeter/views.py
+++ financial_planner/budgeter/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'budgeter/home.html')
+
+def about(request):
+    return render(request, 'budgeter/about.html', {'title': 'About'})
+
+def contact(request):
+    return render(request, 'budgeter/contact.html', {'title': 'Contact'})
