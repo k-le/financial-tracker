ceedeec2139ab5527ff9d96474b447682f93e28d
punyeon <kevin.lle1099@gmail.com>
2021-01-21 19:59:33 -0600

    updated settings.py for static files

--- financial_planner/urls.py
+++ financial_planner/urls.py
@@ -30,8 +30,3 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-else:
-    urlpatterns += patterns('',
-                            (r'^static/(?P<path>.*)$', 'django.views.static.serve',
-                             {'document_root': settings.STATIC_ROOT, 'show_indexes': False}),
-                            )
